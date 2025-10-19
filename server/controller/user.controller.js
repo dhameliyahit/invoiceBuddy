0d631f4719f297f22f0f0a933e9e173b793d39c5
dhameliyahit <heetdhameliya59@gmail.com>
2025-10-19 16:23:26 +0530

    water mark + end message

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -182,16 +182,14 @@ exports.generateInvoiceController = async (req, res) => {
             logo: user.logo, // assuming you store logo URL in DB
         };
 
-        const UserInfo = await userModel.find({ _id: _id });
-
         // 2. Merge business info with request body to generate PDF
         const pdfBuffer = await generateInvoicePdf({
             pdfBusinessData: businessInfo,
             customer: req.body.customer,
             rows: req.body.rows,
             grandDetails: req.body.grandDetails,
-            watermarkText: UserInfo.waterMark || "Invoice Buddy",
-            endMessage:UserInfo.endMessage || "Thank You"
+            watermarkText: user.waterMark || "Invoice Buddy",
+            endMessage: user.endMessage || "Thank You"
         });
 
         // 3. Send PDF as response
